c0ddce92c262a75d18a9b2ea91e223c2434cab00
卢壮壮 <lzz@pcuu.com>
2019-01-21 14:46:52 +0800

    首次代码提交

---
+++ component/child1/index.js
@@ -0,0 +1,30 @@
+// component/child1/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    values:{
+      type: Object,
+      observer(newVal, oldVal, changedPath) {
+        // 属性被改变时执行的函数（可选），也可以写成在methods段中定义的方法名字符串, 如：'_propertyChange'
+        // 通常 newVal 就是新设置的数据， oldVal 是旧数据
+        console.log(newVal)
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
